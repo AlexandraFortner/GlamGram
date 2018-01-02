551126cc82ec0388fd95212cb5074484e201a38e
Alexandra <afortner@basecampcodingacademy.org>
2018-01-02 11:09:44 -0600

    Added more filter choices and uploaded more pictures.

--- app/app/form.py
+++ app/app/form.py
@@ -5,7 +5,19 @@ from app.models import Photo
 
 
 class PhotoForm(forms.ModelForm):
-    FILTER_CHOICES = [('blur', 'Blur'), ('sharpen', 'Sharpen')]
+    FILTER_CHOICES = [
+        ('none', 'None'),
+        ('blur', 'Blur'),
+        ('sharpen', 'Sharpen'),
+        ('contour', 'Contour'),
+        ('detail', 'Detail'),
+        ('edge_enhance', 'Edge Enhance'),
+        ('edge_enhance_more', 'Edge Enhance More'),
+        ('emboss', 'Emboss'),
+        ('find_edges', 'Find Edges'),
+        ('smooth', 'Smooth'),
+        ('smooth_more', 'Smooth More'),
+    ]
     filterChoice = forms.CharField(
         label='Filters:', widget=forms.Select(choices=FILTER_CHOICES))
 
